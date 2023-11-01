cb34bc73998cbd6fedf29bad90cccbf125a23842
baba-ott <inig0001@hz.nl>
2023-11-01 14:20:18 +0100

    adding testing

---
+++ care-bear/src/components/MyComponent.svelte
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/svelte';
+import MyComponent from './MyComponent.svelte';
+
+describe('MyComponent', () => {
+  test('should render', () => {
+    const { getByText } = render(MyComponent, { props: { name: 'World' } });
+
+    expect(getByText('Hello World')).toBeInTheDocument();
+  });
+});
