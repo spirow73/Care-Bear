d9eb3642cbf8d496d720167f4ebe62de49861ace
ok <zajt0001@hz.nl>
2023-11-02 10:26:13 +0100

    fixed errors

--- care-bear/src/components/Button.svelte
+++ care-bear/src/components/Button.svelte
@@ -2,7 +2,7 @@
 <script>
 	export let text = 'Button';
 	export let onClick;
-	let classes = 'bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded';
+	let classes = 'bg-grey-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded';
 </script>
 
 <button on:click={onClick} class={classes}>{text}</button>
