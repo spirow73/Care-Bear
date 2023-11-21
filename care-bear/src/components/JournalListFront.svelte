e3dbe00fb4d896cf42431270fc7e11a656a1c1b8
baba-ott <inig0001@hz.nl>
2023-11-21 13:56:09 +0100

    calender added

---
+++ care-bear/src/components/JournalListFront.svelte
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Journal Home</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            line-height: 1.6;
+        }
+
+        .container {
+            width: 80%;
+            margin: auto;
+            padding: 20px;
+        }
+
+        h1, h2 {
+            text-align: center;
+        }
+
+        button {
+            display: block;
+            width: max-content;
+            margin: 20px auto;
+            background-color: #4a90e2;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+            padding: 10px 20px;
+        }
+
+        button:hover {
+            background-color: #357abd;
+        }
+
+        #recent-entries .entry {
+            border-bottom: 1px solid #ddd;
+            padding: 10px;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Your Journal</h1>
+        <p>Reflect on your past entries or create a new one.</p>
+
+        <button id="new-entry-button" onclick="location.href='journal-entry.html';">New Journal Entry</button>
+
+        <h2>Recent Entries</h2>
+        <div id="recent-entries">
+            <!-- Previous entries will be loaded here dynamically -->
+            <div class="entry">
+                <h3>Entry Date: 2023-11-18</h3>
+                <p>Today was a challenging day, but I managed to get through it with some meditation and a long walk...</p>
+            </div>
+            <!-- More entries can be added here -->
+        </div>
+    </div>
+
+    <script>
+        // JavaScript for dynamic loading and interaction (if necessary)
+    </script>
+</body>
+</html>
--- care-bear/src/routes/calender/+page.svelte
+++ care-bear/src/routes/calender/+page.svelte
@@ -3,42 +3,116 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calendar</title>
+    <title>Four-Week Calendar</title>
     <style>
-        /* CSS styles will be added here */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: white;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .calendar-header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px;
+            background-color: #4a90e2;
+            color: white;
+        }
+
+        .calendar-header button {
+            background: none;
+            border: none;
+            color: white;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        .weekdays {
+            display: flex;
+            background-color: #eee;
+            text-align: center;
+        }
+
+        .weekdays div {
+            flex: 1;
+            padding: 10px;
+            font-weight: bold;
+        }
+
+        .days {
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        .day {
+            width: calc(100% / 7);
+            padding: 15px;
+            border: 1px solid #ddd;
+            text-align: center;
+            min-height: 100px;
+            box-sizing: border-box;
+        }
     </style>
 </head>
 <body>
     <div class="container">
-        <h1>Your Calendar</h1>
-        <div class="calendar">
-            <!-- Calendar Header -->
-            <div class="calendar-header">
-                <button id="prev-month">Previous</button>
-                <span id="month-year">Month Year</span>
-                <button id="next-month">Next</button>
-            </div>
-
-            <!-- Days of the Week -->
-            <div class="weekdays">
-                <div>Sun</div>
-                <div>Mon</div>
-                <div>Tue</div>
-                <div>Wed</div>
-                <div>Thu</div>
-                <div>Fri</div>
-                <div>Sat</div>
-            </div>
-
-            <!-- Calendar Days -->
-            <div class="days">
-                <!-- Days will be generated dynamically here -->
-            </div>
+        <div class="calendar-header">
+            <span id="month-year">Four Week Overview</span>
+        </div>
+        <div class="weekdays">
+            <div>Sun</div>
+            <div>Mon</div>
+            <div>Tue</div>
+            <div>Wed</div>
+            <div>Thu</div>
+            <div>Fri</div>
+            <div>Sat</div>
+        </div>
+        <div class="days">
+            <!-- Week 1 -->
+            <div class="day">1</div>
+            <div class="day">2</div>
+            <div class="day">3</div>
+            <div class="day">4</div>
+            <div class="day">5</div>
+            <div class="day">6</div>
+            <div class="day">7</div>
+            <!-- Week 2 -->
+            <div class="day">8</div>
+            <div class="day">9</div>
+            <div class="day">10</div>
+            <div class="day">11</div>
+            <div class="day">12</div>
+            <div class="day">13</div>
+            <div class="day">14</div>
+            <!-- Week 3 -->
+            <div class="day">15</div>
+            <div class="day">16</div>
+            <div class="day">17</div>
+            <div class="day">18</div>
+            <div class="day">19</div>
+            <div class="day">20</div>
+            <div class="day">21</div>
+            <!-- Week 4 -->
+            <div class="day">22</div>
+            <div class="day">23</div>
+            <div class="day">24</div>
+            <div class="day">25</div>
+            <div class="day">26</div>
+            <div class="day">27</div>
+            <div class="day">28</div>
         </div>
     </div>
-
-    <script>
-        // JavaScript code will go here
-    </script>
 </body>
 </html>
