27fcfdc5729c341b2e127cc5b1a9bcdb4116de04
ok <zajt0001@hz.nl>
2023-11-21 11:07:47 +0100

    fixed the navigation

--- care-bear/src/routes/+layout.svelte
+++ care-bear/src/routes/+layout.svelte
@@ -1,4 +1,34 @@
 <script>
 	import '../app.css';
 </script>
+<nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+	<div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+		<div class="text-sm lg:flex-grow">
+			<a
+				href="/"
+				class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                
+                Home
+            </a>
+			<a
+				href="/journal"
+				class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+			>
+				Journal
+			</a>
+			<a
+				href="/tasks"
+				class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+			>
+				Tasks
+			</a>
+			<a
+				href="/mood"
+				class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+			>
+				Mood
+			</a>
+		</div>
+	</div>
+</nav>
 <slot />
--- care-bear/src/routes/mood/+page.svelte
+++ care-bear/src/routes/mood/+page.svelte
@@ -1,46 +1,62 @@
-<!-- MoodPage.svelte -->
-
 <script>
-    let selectedMood = "";
-  
-    function selectMood(mood) {
-      selectedMood = mood;
-    }
-  </script>
-  
-  <style>
-    /* Add your Svelte styles here */
-  </style>
-  <main class="max-w-md  mt-10 p-6 ">
-    <h1 class="text-2xl font-bold mb-4">How are you feeling today?</h1>
-    <div class="flex">
-      <div
-        class="{ 'cursor-pointer p-4 m-2 border rounded ' + (selectedMood === 'happy' ? 'bg-blue-500' : 'bg-gray-500') }"
-        on:click={() => selectMood('happy')}
-      >
-      <img class="h-auto max-w-xs"src="https://web-holidays.com/wp-content/uploads/2020/06/emoji.png" alt="image description">
-      
-        Happy
-      </div>
-      <div
-        class="{ 'cursor-pointer p-4 m-2 border rounded ' + (selectedMood === 'sad' ? 'bg-blue-800' : 'bg-gray-500') }"
-        on:click={() => selectMood('sad')}
-      >
-      <img class="h-auto max-w-xs ms-auto" src="http://cdn.shopify.com/s/files/1/1061/1924/products/Emoji_Icon_-_Sad_Emoji_grande.png?v=1542436016" alt="image description">
+	let selectedMood = null;
 
-        Sad
-      </div>
-      <div
-        class="{ 'cursor-pointer p-4 m-2 border rounded ' + (selectedMood === 'stressed' ? 'bg-blue-900' : 'bg-gray-500') }"
-        on:click={() => selectMood('stressed')}
-      >
-      <img class="h-auto max-w-xl mx-auto" src="https://emoji.gg/assets/emoji/5721_NYOOO.png" alt="image description">
+	const selectMood = (mood) => {
+		selectedMood = selectedMood === mood ? null : mood;
+	};
+</script>
 
-        Stressed
-      </div>
-    </div>
-    {#if selectedMood !== ""}
-      <p class="mt-4">You selected: {selectedMood}</p>
-    {/if}
-  </main>
-  
+<main class="max-w-md mx-auto mt-10 p-6">
+	<h1 class="text-2xl font-bold mb-4">How are you feeling today?</h1>
+	<div class="flex">
+		<div class="flex flex-wrap justify-center">
+			<div
+				class={'cursor-pointer p-4 m-2 border rounded ' +
+					(selectedMood === 'happy' ? 'bg-blue-500' : 'bg-gray-500')}
+				on:click={() => selectMood('happy')}
+			>
+				{#if selectedMood === 'happy'}
+					<img
+						class="h-auto max-w-xs md:max-w-lg rounded-lg"
+						src="https://web-holidays.com/wp-content/uploads/2020/06/emoji.png"
+						alt="Happy emoji"
+					/>
+				{/if}
+				<div class="mt-2 text-center">Happy</div>
+			</div>
+
+			<div
+				class={'cursor-pointer p-4 m-2 border rounded ' +
+					(selectedMood === 'sad' ? 'bg-blue-800' : 'bg-gray-500')}
+				on:click={() => selectMood('sad')}
+			>
+				{#if selectedMood === 'sad'}
+					<img
+						class="h-auto max-w-xs md:max-w-lg rounded-lg"
+						src="http://cdn.shopify.com/s/files/1/1061/1924/products/Emoji_Icon_-_Sad_Emoji_grande.png?v=1542436016"
+						alt="Sad emoji"
+					/>
+				{/if}
+				<div class="mt-2 text-center">Sad</div>
+			</div>
+
+			<div
+				class={'cursor-pointer p-4 m-2 border rounded ' +
+					(selectedMood === 'stressed' ? 'bg-blue-900' : 'bg-gray-500')}
+				on:click={() => selectMood('stressed')}
+			>
+				{#if selectedMood === 'stressed'}
+					<img
+						class="h-auto max-w-xs md:max-w-lg rounded-lg"
+						src="https://emoji.gg/assets/emoji/5721_NYOOO.png"
+						alt="Stressed emoji"
+					/>
+				{/if}
+				<div class="mt-2 text-center">Stressed</div>
+			</div>
+		</div>
+	</div>
+	{#if selectedMood !== ''}
+		<p class="mt-4">You selected: {selectedMood}</p>
+	{/if}
+</main>
