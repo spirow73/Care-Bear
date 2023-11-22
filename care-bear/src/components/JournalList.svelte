5a5d76aa1d55d50e1f7f6724d33812277087518f
KrackenDome <112581959+KrackenDome@users.noreply.github.com>
2023-11-22 10:10:46 +0100

    added some simple css to the journal stuff

--- care-bear/src/components/JournalList.svelte
+++ care-bear/src/components/JournalList.svelte
@@ -1,25 +1,51 @@
 <script>
-  import { journalEntryStore } from '$lib/journalEntryStore.js';
+  import { journalEntryStore } from '$lib/journalEntryStore';
   import { onMount } from 'svelte';
 
-  let journalEntries = [];
+  let entries = [];
 
   onMount(async () => {
-    const unsubscribe = journalEntryStore.subscribe(entries => {
-      journalEntries = entries;
-    });
-    await journalEntryStore.fetchEntries(1); 
-    return unsubscribe;
+    await journalEntryStore.fetchJournalEntries(1); // Assuming 1 as journal ID
   });
 
   function selectEntry(entryId) {
-    
+    // Handle selecting the entry
+    // You can perform actions here when an entry is selected
   }
 </script>
 
+<style>
+  /* CSS styles for the Journal Entries component */
+  h1 {
+    font-size: 24px;
+    margin-bottom: 16px;
+    color: #333;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    padding: 8px 16px;
+    margin-bottom: 8px;
+    background-color: #f5f5f5;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  li:hover {
+    background-color: #e0e0e0;
+  }
+</style>
+
+<h1>Journal Entries</h1>
+
 <ul>
- 
-  {#each journalEntries as entry}
+  {#each entries as entry (entry.id)}
     <li on:click={() => selectEntry(entry.id)}>
       {entry.title}
     </li>
