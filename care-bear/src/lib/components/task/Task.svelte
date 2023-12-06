2a98f18b262d921a7464ddd2ec8286bb8c4526d2
spirow73 <67395826+spirow73@users.noreply.github.com>
2023-12-06 14:32:21 +0100

    Added the status functionality to tasks

--- care-bear/src/lib/components/task/Task.svelte
+++ care-bear/src/lib/components/task/Task.svelte
@@ -1,7 +1,7 @@
 <script>
 	import TaskView from './TaskView.svelte';
 	import TaskEdit from './TaskEdit.svelte';
-	import { removeDbTask } from '$lib/taskStore';
+	import { removeDbTask, toggleTaskCompletion } from '$lib/taskStore';
 	export let task_id;
 	export let title;
 	export let description;
@@ -24,13 +24,29 @@
 	function toggleEdit() {
 		isEditing = !isEditing;
 	}
+
+	function handleToggleCompletion() {
+		toggleTaskCompletion(task_id, isCompleted);
+	}
 </script>
 
-<div>
+<div class="flex items-center mb-2 bg-brown-900 shadow-md p-4 rounded-full border border-black">
+	<div
+		class="{isCompleted
+			? 'bg-green-500'
+			: 'bg-red-500'} h-6 w-6 rounded-full flex items-center justify-center mr-4"
+		on:click={handleToggleCompletion}
+	/>
+
 	{#if isEditing}
-		<TaskEdit {task} />
+		<TaskEdit {title} {description} {deadline} {task_id} closeEdit={toggleEdit} />
 	{:else}
-		<TaskView {task} />
+		<TaskView {title} {description} {formattedDeadline} />
 	{/if}
-	<button on:click={toggleEdit}>Edit</button>
+
+	<button on:click={toggleEdit} class="ml-2"> 🖊️ </button>
+
+	<button on:click={handleDelete} class="text-red-500 hover:text-red-700 ml-auto">
+		<span class="text-red-500">❌</span>
+	</button>
 </div>
