e046d8a5f20c6ae10f29478620b17d0fe3770185
spirow73 <67395826+spirow73@users.noreply.github.com>
2023-12-29 03:01:33 +0100

    Added dark mode support

--- care-bear/src/routes/+layout.svelte
+++ care-bear/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import '../app.css';
 	import profile from '$lib/components/images/profile.png';
+	import DarkModeButton from '$lib/components/generic/DarkModeButton.svelte';
 	const animations = {
 		enter: (node) => {
 			node.animate(
@@ -32,26 +33,26 @@
 	let isMenuOpen = false;
 
 	const toggleMenu = () => {
-    isMenuOpen = !isMenuOpen;
-    const mobileNav = document.querySelector('.mobilenav');
-    mobileNav.classList.toggle('hidden');
-  };
+		isMenuOpen = !isMenuOpen;
+		const mobileNav = document.querySelector('.mobilenav');
+		mobileNav.classList.toggle('hidden');
+	};
 
-  onMount(() => {
-    const menuButton = document.querySelector('.menu-button');
-    const mobileNav = document.querySelector('.mobilenav');
+	onMount(() => {
+		const menuButton = document.querySelector('.menu-button');
+		const mobileNav = document.querySelector('.mobilenav');
 
-    menuButton.addEventListener('click', () => {
-      toggleMenu();
-    });
+		menuButton.addEventListener('click', () => {
+			toggleMenu();
+		});
 
-    // Close mobile menu when a link is clicked
-    mobileNav.querySelectorAll('a').forEach((link) => {
-      link.addEventListener('click', () => {
-        toggleMenu();
-      });
-    });
-  });
+		// Close mobile menu when a link is clicked
+		mobileNav.querySelectorAll('a').forEach((link) => {
+			link.addEventListener('click', () => {
+				toggleMenu();
+			});
+		});
+	});
 </script>
 
 <div class="min-h-screen">
@@ -85,14 +86,13 @@
 
 			<div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mobilenav">
 				<div class="text-sm lg:flex-grow">
-					<a  
+					<a
 						href="/"
 						class="transition-all block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"
 						use:animations.enter
 						use:animations.leave
 					>
-					
-				            Home
+						Home
 					</a>
 
 					<a
@@ -130,7 +130,6 @@
 					>
 						Calendar
 					</a>
-
 					<a
 						href="/profile"
 						class="transition-all block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"
@@ -138,43 +137,30 @@
 						use:animations.leave
 					>
 						<!-- Profile -->
-						<img src="{profile}" alt="circle with a silouet" class="h-6 w-6 ml-2 relative top-1.5" />
+						<img src={profile} alt="circle with a silouet" class="h-6 w-6 ml-2 relative top-1.5" />
 					</a>
+					<DarkModeButton />
 				</div>
 			</div>
 		</div>
 	</nav>
-	
+
 	<slot />
-	
 </div>
 
 <!-- <body class="bg-cover bg-center" style="background-image: url('https://img.freepik.com/premium-photo/dreamy-ocean-coast-summer_360074-15698.jpg');"> -->
 <!-- </body> -->
 
-
-
 <style>
-.light {
-  background-color: white;
-  color: black;
-}
-
-.dark {
-  background-color: black;
-  color: white;
-
-}
-.cotton-candy {
-			background: linear-gradient(95deg, rgb(249, 196, 232), rgb(229, 215, 255), rgb(171, 219, 254));
-		}
-		:global(body) {
-	background-image: url('$lib/components/images/background.png');
-    background-size: cover; /* Adjust the sizing as needed */
-    background-repeat: no-repeat;
-	background-attachment: fixed;
-	height: 100vh;
-    margin: 0;
-  }
+	.cotton-candy {
+		background: linear-gradient(95deg, rgb(249, 196, 232), rgb(229, 215, 255), rgb(171, 219, 254));
+	}
+	:global(body) {
+		background-image: url('$lib/components/images/background.png');
+		background-size: cover; /* Adjust the sizing as needed */
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		height: 100vh;
+		margin: 0;
+	}
 </style>
-
