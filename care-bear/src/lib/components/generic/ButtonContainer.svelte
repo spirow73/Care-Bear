9183945e3e75539ba8147cc7469907de61fadcfd
spirow73 <67395826+spirow73@users.noreply.github.com>
2024-01-09 23:25:28 +0100

    Changes to build properly the project

--- care-bear/src/lib/components/generic/ButtonContainer.svelte
+++ care-bear/src/lib/components/generic/ButtonContainer.svelte
@@ -2,8 +2,8 @@
 	import Button from './Button.svelte';
 	export let primaryText = 'Primary text';
 	export let secondaryText = 'Secondary text';
-	export let primaryHandle: () => void;
-	export let secondaryHandle: () => void; // Explicitly define the type of secondaryHandle prop
+	export let primaryHandle;
+	export let secondaryHandle; // Explicitly define the type of secondaryHandle prop
 </script>
 
 <div class="flex flex-col md:flex-row">
