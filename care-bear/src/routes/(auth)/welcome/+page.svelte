b48b37fd58c2684111ca9b0990780f923a57be37
spirow73 <67395826+spirow73@users.noreply.github.com>
2024-01-16 18:45:52 +0100

    login changed

--- care-bear/src/routes/(auth)/welcome/+page.svelte
+++ care-bear/src/routes/(auth)/welcome/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import ButtonContainer from '$lib/components/generic/ButtonContainer.svelte';
 	import { goto } from '$app/navigation';
+	import bear from '$lib/components/images/bear.png';
 
 	function handleLoginClick(event) {
 		event.preventDefault(); // Prevenir el comportamiento predeterminado
@@ -13,8 +14,13 @@
 	}
 </script>
 
-<div class="flex flex-col items-center justify-center min-h-screen m-4">
-	<h1 class="text-5xl font-bold text-gray-800 mb-10 text-center">Welcome to Care Bear</h1>
+<div class="flex flex-col items-center justify-center min-h-screen">
+	<h1 class="text-5xl font-bold text-gray-800 mb-5 text-center">Welcome to Care Bear</h1>
+
+	<div class="object-none object-top mb-5">
+		<img class="max-w-full max-h-full" src={bear} alt={bear} />
+	</div>
+
 	<ButtonContainer
 		primaryText="Login"
 		secondaryText="Get Started"
