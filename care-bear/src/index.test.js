40e0a4b57b3675cf879c4fef602ebad59596c11e
spirow73 <67395826+spirow73@users.noreply.github.com>
2024-01-25 14:16:25 +0100

    fixed the docker build
    :c

--- care-bear/src/index.test.js
+++ care-bear/src/index.test.js
@@ -1,8 +1,8 @@
 // Create a jest
-
+import { describe, expect, test } from 'vitest';
 
 describe('First test', () => {
 	test('should pass', () => {
-	  expect(true).toBe(true);
+		expect(true).toBe(true);
 	});
-  });
+});
